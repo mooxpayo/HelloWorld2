c2dec8c8838099efef0c076e67838840f995ec12
Kanako motoki <mooook09sktm@gmail.com>
2018-11-09 20:10:35 +0900

    aaaa

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by 元木 加奈子 on 2018/11/09.
+//  Copyright © 2018 元木 加奈子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
